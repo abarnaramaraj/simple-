89493dea661736e02196cbeaada6beba54672f86
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-07 09:44:58 +0530

    Create Simpleinterest.java

---
+++ Simpleinterest.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+ public class Simpleinterest
+ {
+  public static void main(String[] args) {
+    int p,n,r,i;
+    Scanner sc=new Scanner(System.in);
+    p=sc.nextInt();
+    n=sc.nextInt();
+    r=sc.nextInt();
+    i=((p*n*r)/100);
+    System.out.println(i);
+    }
+  }
